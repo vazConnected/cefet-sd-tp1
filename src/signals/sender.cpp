9cb5318df789b4f56966c9bced76f23d5864acdd
Pedro Vaz <vaz.connected@gmail.com>
2026-08-30 16:46:12 -0300

    feat: create sender

---
+++ src/signals/sender.cpp
@@ -0,0 +1,12 @@
+#include <csignal>      // kill, SIGTERM
+#include <cerrno>       // errno, ESRCH, EPERM
+#include <cstdio>       // fprintf, stderr
+#include <cstdlib>      // strtol
+#include <unistd.h>     // pid_t
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
+
